3948fe2c1a18020be463b5e4b9a1e803f0210044
luvnft <140175751+luvnft@users.noreply.github.com>
2025-05-17 06:55:03 -0400

    Update DataOverview.vue

--- client/src/presentation/components/DataOverview/DataOverview.vue
+++ client/src/presentation/components/DataOverview/DataOverview.vue
@@ -12,9 +12,7 @@ defineProps<{
 
 <template>
   <div class="data-table">
-    <DataOverviewItem
-      :title="`${rating.votesCount} ratings`"
-    >
+    <DataOverviewItem :title="`${rating.votesCount} ratings`">
       <template #content>
         <Amount>
           {{ rating.rating }}
@@ -22,28 +20,22 @@ defineProps<{
       </template>
 
       <template #footer>
-        <!-- eslint-disable vue/no-v-html -->
-        <template
-          v-for="index in 5"
-          :key="index"
-        >
-          {{ index <= rating.rating ? '★' : '☆' }}
+        <template v-for="index in 5" :key="index">
+          <span aria-hidden="true" class="star">
+            {{ index <= Math.round(rating.rating) ? '★' : '☆' }}
+          </span>
         </template>
       </template>
     </DataOverviewItem>
 
     <div class="divider" />
 
-    <DataOverviewItem
-      title="Awards"
-    >
+    <DataOverviewItem title="Awards">
       <template #content>
         <div class="award">
-          <Icon name="laurel-left" />
-          <div class="award-text">
-            {{ award.name }}
-          </div>
-          <Icon name="laurel-right" />
+          <Icon name="laurel-left" class="icon" />
+          <div class="award-text">{{ award.name }}</div>
+          <Icon name="laurel-right" class="icon" />
         </div>
       </template>
 
@@ -54,9 +46,7 @@ defineProps<{
 
     <div class="divider" />
 
-    <DataOverviewItem
-      title="Charts"
-    >
+    <DataOverviewItem title="Charts">
       <template #content>
         <Amount prefix="No.">
           {{ chart.place }}
@@ -73,37 +63,46 @@ defineProps<{
 <style scoped>
 .data-table {
   display: grid;
-  grid-template-columns: fit-content(100%) 1px fit-content(100%) 1px fit-content(100%);
+  grid-template-columns: repeat(3, fit-content(100%)) /* three items */
+                        / 1px 1px; /* two dividers */
   grid-gap: 8px;
   align-items: center;
   padding-block: 10px;
   justify-content: center;
+}
 
-  .divider {
-    height: 40px;
-    width: 0.5px;
-    background-color: var(--separator-color);
+.divider {
+  height: 40px;
+  width: 0.5px;
+  background-color: var(--separator-color);
+}
+
+:deep(.number) {
+  font-size: 23px;
+}
+
+.award {
+  display: flex;
+  align-items: center;
+  gap: 6px;
+  font-size: 10px;
+
+  &-text {
+    max-width: 40px;
+    text-align: center;
   }
 
-  :deep(.number){
-    font-size: 23px;
+  .icon {
+    width: 11px;
+    height: 23px;
+    fill: var(--color-hint);
   }
+}
 
-  .award {
-    display: flex;
-    align-items: center;
-    gap: 4px;
-    font-size: 10px;
-
-    &-text {
-      max-width: 40px;
-      text-align: center;
-    }
-    .icon {
-      width: 11px;
-      height: 23px;
-      fill: var(--color-hint);
-    }
-  }
+.star {
+  font-size: 16px;
+  color: var(--color-link);
+  user-select: none;
+  margin-right: 2px;
 }
 </style>
